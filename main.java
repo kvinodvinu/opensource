43a7c96fde5fda18dde41b17d72e767be3879810
kvinodvinu <kvinod9315@gmail.com>
2024-11-12 10:31:06 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Solution {
+  public static void main(string[] args) {
+    Scanner scaneer = new Scanner(System.in);
+    int X = scanner.nextInt();
+    int Y = scanner.nextInt();
+    sum = X+Y;
+    System.out.println(sum);
+  }
+}
+      
+  
